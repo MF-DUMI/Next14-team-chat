c8a7a796f8ed09d2375eee984fff88d7d0214b24
Ogua D. Leslie <ogua@ualberta.ca>
2024-09-02 23:06:25 -0600

    h

---
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from "@/features/auth/components/user-button"
+import { WorkspaceSwitcher } from "./workspace-switcher"
+import { SidebarButton } from "./sidebar-button"
+import { BellIcon, Home, MessageSquare, MessagesSquare, MoreHorizontal } from "lucide-react"
+import { usePathname } from "next/navigation"
+
+export const Sidebar = () => {
+
+    const pathname = usePathname();
+
+
+    return (
+        <aside className="bg-[#481349] w-[70px] h-full flex flex-col gap-y-4 items-center pt-[9px] pb-4">
+            <WorkspaceSwitcher />
+            <SidebarButton icon={Home} label="Home" isActive={pathname.includes("/workspace")} />
+            <SidebarButton icon={MessagesSquare} label="DMs"  />
+            <SidebarButton icon={BellIcon} label="Activity"  />
+            <SidebarButton icon={MoreHorizontal} label="More"  />
+            <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+                <UserButton/>
+            </div>
+        </aside>
+    )
+}
---
+++ src/components/modals.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+
+import { CreateWorkspaceModal } from "@/features/workspace/components/create-workspace-modal";
+import { useEffect, useState } from "react";
+import { CreateChannelModal } from "@/features/channels/components/create-channel-modal";
+
+
+
+export const Modals= () =>{
+
+    const [mounted, setMounted] = useState(false);
+        
+    useEffect(() => {
+        setMounted(true);
+    }, [])
+
+    if (!mounted) return null;
+
+
+
+    return (
+        <>
+            <CreateChannelModal/>
+            <CreateWorkspaceModal/>
+        </>
+    )
+}
